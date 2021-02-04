1ed954451d05310f400c31c697db2062f120ac54
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-02-04 10:48:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+let num1 = 31
+let num2 = 2
+var multiply = num1 * num2
+
+var random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 8
+let num4 = 2
+var mod = num3 / num4
+
+var max = Math.max(1, 2, 3, 4, 20);
